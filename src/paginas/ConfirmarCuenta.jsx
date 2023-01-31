ec87de9621442113aeb815113a8228a51672edf4
zassory <nicolas.programador@gmail.com>
2023-01-31 20:26:36 -0300

    confirmar cuenta creado ok

--- src/paginas/ConfirmarCuenta.jsx
+++ src/paginas/ConfirmarCuenta.jsx
@@ -2,6 +2,8 @@
 
 export const ConfirmarCuenta = () => {
   return (
-    <div>ConfirmarCuenta</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">Confirma tu cuenta y Comienza a crear tus {''}<span className="text-slate-700">proyectos</span></h1>
+    </>
   )
 }

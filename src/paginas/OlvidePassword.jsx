c014c48f28dd683601566a908da334b741fb661b
zassory <nicolas.programador@gmail.com>
2023-02-02 21:29:57 -0300

    Validacion en olvide password si campo esta vacio con componente de alerta

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,11 +1,34 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Alerta } from "../components/Alerta";
 export const OlvidePassword = () => {
+
+  const [email,setEmail] = useState('');
+  const [alerta,setAlerta] = useState({});
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if(email === '' || email.length < 6){
+      setAlerta({
+        msg:'El Email es Obligatorio',
+        error:true
+      });
+      return;
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
     <h1 className="text-sky-600 font-black text-6xl capitalize">Recupera tu acceso y no pierdas tus <span className="text-slate-700">proyectos</span></h1>
 
-    <form className="my-10 bg-white shadow rounded-lg p-10">
+    { msg && (<Alerta alerta={alerta} />) }
+
+    <form 
+      className="my-10 bg-white shadow rounded-lg p-10"
+      onSubmit={handleSubmit}  
+    >
      
       <div className="my-5">
         <label 
@@ -17,6 +40,8 @@ export const OlvidePassword = () => {
           type="email"
           placeholder="Email de Registro"
           className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
       </div>      
       <input 
@@ -26,7 +51,7 @@ export const OlvidePassword = () => {
         rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
       />
     </form>
-
+    
     <nav className="lg:flex lg:justify-between">
       <Link
         to="/"

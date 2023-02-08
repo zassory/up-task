6b710c075b5c8c815e19f8ac8c484f5226161f72
zassory <nicolas.programador@gmail.com>
2023-02-08 19:36:53 -0300

    context creado

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthLayout } from './layouts/AuthLayout';
 import { Login , Registrar , OlvidePassword , NuevoPassword , ConfirmarCuenta } from './paginas';
 
+import { AuthProvider } from './context/AuthProvider';
+
 function App() {
   
 
-  return (
+  return (    
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="registrar" element={<Registrar />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword />} />
-          <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-        </Route>        
-      </Routes>
-    </BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+          </Route>        
+        </Routes>
+        </AuthProvider>
+    </BrowserRouter>    
   )
 }
 

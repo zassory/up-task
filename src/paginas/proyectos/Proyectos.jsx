28e4abc8688d4b93d6cb2eb5c661c05bb9e99fb9
zassory <nicolas.programador@gmail.com>
2023-02-13 18:39:55 -0300

    Guardando proyectos en el state y ya se obtienen desde proyecto

--- src/paginas/proyectos/Proyectos.jsx
+++ src/paginas/proyectos/Proyectos.jsx
@@ -2,14 +2,16 @@ import useProyectos from "../../hooks/useProyectos";
 
 export const Proyectos = () => {
 
-  const { proyectos } = useProyectos();  
+  const { proyectos } = useProyectos();
+
+  console.log(proyectos);
 
   return (
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
 
-      <div>
-        
+      <div className="bg-white shadow mt-10 rounded-lg p-5">
+        {proyectos.length ? <p>Si hay Proyectos</p> : <p className="mt-5 text-center text-gray-600 uppercase">No hay proyectos aún</p>}
       </div>
     </>
   )

788f3aa1f7f962d23e0ea57efb7b338e25ab7422
zassory <nicolas.programador@gmail.com>
2023-02-17 17:06:53 -0300

    correccion en el login, boton de agregar tareas en el proyecto

--- src/paginas/proyectos/Proyecto.jsx
+++ src/paginas/proyectos/Proyecto.jsx
@@ -14,25 +14,10 @@ export const Proyecto = () => {
 
   const { nombre } = proyecto;
 
+  if(cargando) return 'Cargando...';
+
   return (
-    cargando ? (
-      <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-        <div className="animate-pulse flex space-x-4">
-          <div className="rounded-full bg-slate-200 h-10 w-10"></div>
-          <div className="flex-1 space-y-6 py-1">
-            <div className="h-2 bg-slate-200 rounded"></div>
-          <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-slate-200 rounded col-span-2"></div>
-              <div className="h-2 bg-slate-200 rounded col-span-1"></div>
-            </div>
-            <div className="h-2 bg-slate-200 rounded"></div>
-          </div>
-          </div>
-        </div>
-      </div>
-      ) 
-      : (
+    <>
         <div className="flex justify-between">
           <h1 className="font-black text-4xl">{nombre}</h1>
           <div className="flex items-center gap-2 text-gray-400 hover:text-black">
@@ -55,8 +40,29 @@ export const Proyecto = () => {
               to={`/proyectos/editar/${params.id}`}
               className='uppercase font-bold'
             >Editar</Link>
+            </div>
           </div>
-        </div>
+          <button
+            className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400
+            text-white text-center hover:bg-sky-700 mt-5 flex gap-2 items-center justify-center"
+          >
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" 
+              viewBox="0 0 24 24" 
+              strokeWidth={1.5} 
+              stroke="currentColor" 
+              className="w-6 h-6"
+            >
+            <path 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" 
+            />
+            </svg>        
+            Nueva Tarea
+          </button>
+        </>        
     )
-  )
+  
 }
--- src/paginas/usuarios/Login.jsx
+++ src/paginas/usuarios/Login.jsx
@@ -13,7 +13,8 @@ export const Login = () => {
   const [password,setPassword] = useState('');
   const [alerta,setAlerta] = useState('');
 
-  const { setAuth } = useAuth();    
+  const { setAuth } = useAuth();
+  const navigate = useNavigate();
   
   const handleSubmit = async e => {
     e.preventDefault();
@@ -37,6 +38,7 @@ export const Login = () => {
       setAlerta({});
       localStorage.setItem('token',data.token);
       setAuth(data);
+      navigate('/proyectos');
 
     }catch(error){
       console.log(error.response.data.msg);

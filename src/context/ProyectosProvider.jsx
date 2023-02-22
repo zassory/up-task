020c6002febd82732f9dc3eed222a054ddc90040
zassory <nicolas.programador@gmail.com>
2023-02-22 17:29:41 -0300

    buscando colaboradores y funcion en el provider

--- src/context/ProyectosProvider.jsx
+++ src/context/ProyectosProvider.jsx
@@ -13,6 +13,7 @@ const ProyectosProvider = ({children}) => {
     const [modalFormularioTarea,setModalFormularioTarea] = useState(false);
     const [tarea,setTarea] = useState({});
     const [modalEliminarTarea,setModalEliminarTarea] = useState(false);
+    const [colaborador,setColaborador] = useState({});
 
     const navigate = useNavigate();
 
@@ -299,7 +300,32 @@ const ProyectosProvider = ({children}) => {
     }
 
     const submitColaborador = async email => {
-        console.log(email);
+
+        setCargando(true);
+        try{
+
+            const token = localStorage.getItem('token');
+            if(!token)  return;
+        
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.post('/proyectos/colaboradores', {email} , config);
+            setColaborador(data);
+            setAlerta({});
+
+        }catch(error){
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            });
+        } finally {
+            setCargando(false);
+        }
     }
 
 
